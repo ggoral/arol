a47f95fb216434fe034e471eb54f5687a98b6e41
ggoral <goral.gonzalo@gmail.com>
2014-05-30 17:13:13 -0300

    delete commented lines on simple_server

--- simple_server.rb
+++ simple_server.rb
@@ -5,8 +5,6 @@ loop {                         # Servers run forever
   client = server.accept       # Wait for a client to connect
   client.puts(Time.now.ctime)  # Send the time to the client
   client.puts "Closing the connection. Bye!"
-#  entrada = client.gets
-#  puts entrada
   client.close                 # Disconnect from the client
 }
 #leonel@backupnet.com.ar
